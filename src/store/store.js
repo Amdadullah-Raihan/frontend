430e6c8fdfe76e3dd2bfe84378ef784c58dd4955
Amdadul Islam <amdadullahrayhan@gmail.com>
2024-12-29 23:39:23 +0600

    include auth slice

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import themeReducer from './theme/theme.slice';
+import authSlice from './auth/auth.slice';
 
 const store = configureStore({
   reducer: {
     theme: themeReducer,
+    auth: authSlice,
   },
 });
 
