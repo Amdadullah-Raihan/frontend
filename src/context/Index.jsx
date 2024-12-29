e485747c15d05e40ea1e861c8059a7058811081f
Amdadul Islam <amdadullahrayhan@gmail.com>
2024-12-29 23:39:04 +0600

    remove theme context

--- src/context/Index.jsx
+++ src/context/Index.jsx
@@ -1,16 +1,13 @@
 import { AuthProvider } from './AuthContext';
 import { CreateCourseProvider } from './CreateCourseContext';
 import { DashboardProvider } from './DashboardContext';
-import { ThemeProvider } from './ThemeContext';
 
 export const MainContextProvider = ({ children }) => {
   return (
     <AuthProvider>
-      <ThemeProvider>
-        <DashboardProvider>
-          <CreateCourseProvider>{children}</CreateCourseProvider>
-        </DashboardProvider>
-      </ThemeProvider>
+      <DashboardProvider>
+        <CreateCourseProvider>{children}</CreateCourseProvider>
+      </DashboardProvider>
     </AuthProvider>
   );
 };
