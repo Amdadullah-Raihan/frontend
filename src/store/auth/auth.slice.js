4f14324492230ea8bdf487f3a0f8a15d00e33602
Amdadul Islam <amdadullahrayhan@gmail.com>
2024-12-29 23:50:04 +0600

    add token to auth

--- src/store/auth/auth.slice.js
+++ src/store/auth/auth.slice.js
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   user: JSON.parse(localStorage.getItem('user')) || {},
+  token: '',
 };
 
 const authSlice = createSlice({
